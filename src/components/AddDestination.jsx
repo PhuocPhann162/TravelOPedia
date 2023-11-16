4b191a40a5bc5fb0895dd5b1328428d122344490
Phuoc Phan <21520405@gm.uit.edu.vn>
2023-11-16 21:44:14 +0700

    refetch data when add new destination using tags in RTK query

--- src/components/AddDestination.jsx
+++ src/components/AddDestination.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import { useState } from "react";
+import { useAddDestinationMutation } from "../api/destinationAPI";
 
 function AddDestination() {
   const [inputCity, setInputCity] = useState("");
   const [inputCountry, setInputCountry] = useState("");
+  const [addDestinationMutation] = useAddDestinationMutation();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // addDestination 
+    addDestinationMutation({
+      id: Math.random() * 100,
+      city: inputCity,
+      country: inputCountry,
+      daysNeeded: parseInt(Math.random() * 10) + 1,
+    })
+
+    setInputCity("");
+    setInputCountry(""); 
+  }
 
   return (
     <div className="p-4 border">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="row col-8 offset-2">
           <h4>Enter a new destination</h4>
           <div className="col-5 p-1">
@@ -29,7 +45,7 @@ function AddDestination() {
             />
           </div>
           <div className="col-2 p-1">
-            <button className="btn btn-success form-control">Add</button>
+            <button  className="btn btn-success form-control">Add</button>
           </div>
         </div>
       </form>
